84877271e2fc9b1049b1f07eb12a99b0febbbfab
Moishe Lettvin <moishel@gmail.com>
2013-11-30 16:21:24 -0700

    More customization options

--- life.js
+++ life.js
@@ -1,5 +1,9 @@
-function startLife(board, keepHistory, generationsToCompute) {
-  var life = new Life(board, keepHistory);
+function startLife(board) {
+  var life = new Life(board);
+  showGenerations(life);
+}
+
+function showGenerations(life, generationsToCompute) {
   life.render();
 
   generation = 0;
@@ -33,7 +37,31 @@ Life = function(board) {
 
   this.dimensions = [0,0,0,0];
   this.size = 32;
+  this.liveCellColor = 'grey';
+  this.shouldAutoScale = true;
+  this.shouldRememberHistory = false;
 };
+
+Life.prototype.rememberHistory = function(shouldRememberHistory) {
+  this.shouldRememberHistory = shouldRememberHistory;
+  return this;
+};
+
+Life.prototype.setScale = function(scale) {
+  this.scale = scale;
+  return this;
+};
+
+Life.prototype.autoScale = function(shouldAutoScale) {
+  this.shouldAutoScale = shouldAutoScale;
+  return this;
+};
+
+Life.prototype.setLiveCellColor = function(liveCellColor) {
+  this.liveCellColor = liveCellColor;
+  return this;
+}
+
 
 Life.prototype.addCoordinateToBoard = function(coord) {
   this.board[JSON.stringify(coord)] = true;
@@ -43,51 +71,61 @@ Life.prototype.render = function() {
   var canvas = $('#board').get(0);
   var ctx = canvas.getContext('2d');
 
-  // First, determine the scale.
-  for (var coord in this.board) {
-    coord = JSON.parse(coord);
-    this.dimensions[0] = Math.min(this.dimensions[0], coord[0]);
-    this.dimensions[2] = Math.max(this.dimensions[2], coord[0]);
-    this.dimensions[1] = Math.min(this.dimensions[1], coord[1]);
-    this.dimensions[3] = Math.max(this.dimensions[3], coord[1]);
-  }
-
-  // Round dimensions to power of 2
-  var xSize = this.dimensions[2] - this.dimensions[0];
-  var ySize = this.dimensions[3] - this.dimensions[1];
-
-  var minsize = Math.max(xSize, ySize);
-
-  if (minsize > this.size) {
-    while (this.size < minsize) {
-      this.size *= 2;
+  if (this.shouldAutoScale) {
+    // First, determine the scale.
+    for (var coord in this.board) {
+      coord = JSON.parse(coord);
+      this.dimensions[0] = Math.min(this.dimensions[0], coord[0]);
+      this.dimensions[2] = Math.max(this.dimensions[2], coord[0]);
+      this.dimensions[1] = Math.min(this.dimensions[1], coord[1]);
+      this.dimensions[3] = Math.max(this.dimensions[3], coord[1]);
     }
-    console.log('Size: ' + this.size);
-    ctx.clearRect(0,0,1024,1024);
+
+    // Round dimensions to power of 2
+    var xSize = this.dimensions[2] - this.dimensions[0];
+    var ySize = this.dimensions[3] - this.dimensions[1];
+
+    var minsize = Math.max(xSize, ySize);
+
+    if (minsize > this.size) {
+      while (this.size < minsize) {
+        this.size *= 2;
+      }
+      ctx.clearRect(0,0,1024,1024);
+    }
+    this.scale = 1024 / this.size;
+    console.log('size: ' + this.size);
+    console.log('scale: ' + this.scale);
   }
 
-  var scale = 1024 / this.size;
 
-  this.drawBoard(this.oldboard, scale, ctx, true);
-  this.drawBoard(this.board, scale, ctx, false);
+  this.drawBoard(this.oldboard, this.scale, ctx, true);
+  this.drawBoard(this.board, this.scale, ctx, false);
 };
 
 Life.prototype.drawBoard = function(board, scale, ctx, clear) {
   for (var coord in board) {
+    console.log(coord);
     coord = JSON.parse(coord);
     coord[0] += this.size / 2;
     coord[1] += this.size / 2;
+    var cellSize;
+    if (scale > 8) {
+      cellSize = scale - 4;
+    } else {
+      cellSize = scale;
+    }
     if (clear) {
       ctx.clearRect(coord[0] * scale,
                     coord[1] * scale,
-                    scale - 4,
-                    scale - 4);
+                    cellSize,
+                    cellSize);
     } else {
-      ctx.fillStyle = 'grey';
+      ctx.fillStyle = this.liveCellColor;
       ctx.fillRect(coord[0] * scale,
                    coord[1] * scale,
-                   scale - 4,
-                   scale - 4);
+                   cellSize,
+                   cellSize);
     }
   }
 }
